123c0f76a9971728daf887a5d0e6e7a080338048
Whitie <weimann.th@yahoo.com>
2018-11-18 10:56:50 +0100

    Rewrite

--- main.py
+++ main.py
@@ -1,86 +1,74 @@
-from time import sleep
-from subprocess import run
-from os import listdir
-from os.path import isfile, join, exists
-from sys import argv, exit
-from random import random
-from tendo import singleton
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-me = singleton.SingleInstance()
+import os
+import random
+import socket
+import subprocess
+import sys
+import time
 
-runtime = {
-    'backgrounds' : [],
-    'unused' : [],
-    'previousWallpaper' : "",
-    'path' : "",
-    'time' : 60
-}
+from argparse import ArgumentParser
+
+
+VALID_EXTENSIONS = {'.jpg', '.png', '.jpeg', '.gif'}
+SOCKET = '/tmp/i3-wallpapers.sock'
+
 
 # Check if file ends with valid picture extension
-def isPicture(fileName):
-    extensions = ['.jpg', '.png', '.jpeg', '.gif']
-    for extension in extensions:
-        if fileName.endswith(extension):
-            return True
+def is_valid_picture(filename):
+    name, ext = os.path.splitext(filename)
+    return ext.lower() in VALID_EXTENSIONS
 
-    return False
 
-# Change desktop wallpaper
-def changeWallpaper(unused):
-    print("Changing Background")
-    nextIndex = int(random() * 1000) % len(unused)
+def get_picture_list(directory):
+    directory = os.path.abspath(directory)
+    pictures = []
+    for item in os.listdir(directory):
+        full = os.path.join(directory, item)
+        if os.path.isfile(full) and is_valid_picture(full):
+            pictures.append(full)
+    return pictures
 
-    # Check to make sure two wallpapers aren't displayed in a row
-    if runtime["previousWallpaper"] == str(unused[nextIndex]):
-        nextIndex = (nextIndex + 1) % len(unused)
-
-    picture = str(unused[nextIndex])
-    print(picture)
-    run(["feh", "--bg-scale", picture])
-    runtime["previousWallpaper"] = str(unused[nextIndex])
-    unused.pop(nextIndex)
-    print("Sleeping")
-    sleep(runtime["time"])
 
 # Handle Command Line Arguments
-def handleArguments():
-    global runtime
-    for i in range(1, len(argv)):
-        # Time should be specified in minutes
-        if argv[i] == "-t":
-            i += 1
-            num = float(argv[i])
-            runtime["time"] = int(num*60)
-        else:
-            if exists(str(argv[i])):
-                runtime["path"] = argv[i]
-            else:
-                print("Unknown parameter: "+argv[i])
+def handle_arguments():
+    p = ArgumentParser(description='Use directory of pictures as wallpapers.')
+    p.add_argument('directory', help='Directory to use as wallpaper source')
+    p.add_argument('-r', '--random', action='store_true', default=False,
+                   help='Randomize wallpaper order (default: %(default)s)')
+    p.add_argument('-t', '--time', type=int, default=5, help='Minutes '
+                   'to display one wallpaper before change '
+                   '(default: %(default).1f)')
+    args = p.parse_args()
+    print(args)
+    return args
+
+
+def loop(pictures, duration):
+    for pic in pictures:
+        start = time.monotonic()
+        subprocess.run(['feh', '--bg-scale', pic])
+        time.sleep(duration * 60 - (time.monotonic() - start))
+
+
+def main():
+    args = handle_arguments()
+    pictures = get_picture_list(args.directory)
+    if args.random:
+        random.shuffle(pictures)
+    try:
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.bind(SOCKET)
+        while True:
+            loop(pictures, args.time)
+    except OSError:
+        print('Program already running. Exiting now...')
+    except KeyboardInterrupt:
+        print('\rClosing now...')
+    finally:
+        s.close()
+
 
 if __name__ == "__main__":
-    handleArguments()
-    for key in runtime:
-        print(key + " - " + str(runtime[key]))
-    if runtime["path"] == "":
-        exit("A directory path must be specified")
-    dirContents = listdir(runtime["path"])
-
-    for fileName in dirContents:
-        filePath = join(runtime["path"], fileName)
-        if isfile(filePath) and isPicture(fileName):
-            runtime["backgrounds"].append(filePath)
-
-    print("Printing images in given directory")
-    for back in runtime["backgrounds"]:
-        print(back)
-
-    runtime["unused"] = list(runtime["backgrounds"])
-
-    # Main wallpaper switching logic done here
-    while True:
-        # Use random number modulo unused length to pick the first wallpaper
-        # First check to ensure unused list is not empty
-        if len(runtime["unused"]) == 0:
-            runtime["unused"] = list(runtime["backgrounds"])
-
-        changeWallpaper(runtime["unused"])
+    main()
